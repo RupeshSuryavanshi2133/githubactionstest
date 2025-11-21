ac8a6ddde2032e2d34a8063bc206f5a546e3598a
RupeshSuryavanshi2133 <rupesh.suryawanshi@headtler.com>
2025-11-21 12:10:09 +0530

    test

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.use(express.json());
 
 app.get("/", async (req, res) => {
   try {
-    const response = await axios.get("https://dummyjson.com/users", {
+    const response = await axios.get("https://dummyjson.com/uss", {
       timeout: 5000,
     });
     res.json(response.data);
